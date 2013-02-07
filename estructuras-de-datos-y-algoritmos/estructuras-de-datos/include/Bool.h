fd4605f50b3973ca7c06f0e3474ad83051201571
Sergio Revilla <sergio@unmundotecnologico.org>
2013-02-07 19:32:49 +0100

    tipo Bool refactorizado

--- estructuras-de-datos-y-algoritmos/estructuras-de-datos/include/Bool.h
+++ estructuras-de-datos-y-algoritmos/estructuras-de-datos/include/Bool.h
@@ -12,20 +12,25 @@ class Bool {
 public:
 // Generadores
 static Bool Falso, Cierto;
-// Falso .bNot() = Cierto ; Cierto .bNot() = Falso
 
+// Falso .bNot() = Cierto ; Cierto .bNot() = Falso
 Bool bNot() const;
+
 // Cierto .bAnd(x) = x; Falso .and(x) = Falso
 Bool bAnd(Bool &otro) const;
+
 // Cierto.bOr(x) = Cierto; Falso.or(x) = x
 Bool bOr(Bool &otro) const;
 
 private:
 // tipo interno
 char _b;
+
 // constructor por defecto : privado
 Bool(char b): _b(b) {}
+
 // amigos: permiten a una funcion externa acceder a privados
 friend std::ostream &operator<<(std::ostream &out, const Bool b);
+
 };
 #endif
